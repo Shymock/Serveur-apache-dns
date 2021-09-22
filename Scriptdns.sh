fb97d38a18c47496c5d93347a1617782f7590068
Jason Serrurier <61480249+Shymock@users.noreply.github.com>
2021-09-22 12:31:29 +0200

    Update Scriptdns.sh

--- Scriptdns.sh
+++ Scriptdns.sh
@@ -1,11 +1,13 @@
 apt update -y
 apt install bind9 -y
 cd /etc
+rm host.conf
+rm resolv.conf
 wget https://raw.githubusercontent.com/Shymock/Serveur-apache-dns/main/host.conf
 wget https://raw.githubusercontent.com/Shymock/Serveur-apache-dns/main/resolv.conf
 cd bind/
+rm named.conf.local
 wget https://raw.githubusercontent.com/Shymock/Serveur-apache-dns/main/db.gsb.lan
 wget https://raw.githubusercontent.com/Shymock/Serveur-apache-dns/main/db.gsb.lan.rev
 wget https://raw.githubusercontent.com/Shymock/Serveur-apache-dns/main/named.conf.local
-wget https://raw.githubusercontent.com/Shymock/Serveur-apache-dns/main/named.conf.default-zones
 systemctl restart bind9
